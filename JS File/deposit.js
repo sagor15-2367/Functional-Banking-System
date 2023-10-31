4cba08a8cd15a717d81661669f04c487dc4f8cef
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-11-01 02:39:41 +0600

    first commit

--- JS File/deposit.js
+++ JS File/deposit.js
@@ -1,1 +1,21 @@
-console.log(utility);
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    // Normal way
+    // 1. get the value element id
+    // 2.get the value from the element
+    // 3. convert string value to a number
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+    // 1. get previous deposit total by id
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+    // console.log(newDepositAmount);
+    // 1.calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    // set deposit total value
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    // get previous balance by using the function
+    const previousBalanceTotal=getTextElementValueById('balance-total');
+    const newBalanceTotal=previousBalanceTotal+newDepositAmount;
+
+    setTextElementValueById('balance-total',newBalanceTotal);
+})
