286ebceacb928c0fe27c5b66880d1b5cc4edd4bf
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-11-01 03:24:18 +0600

    first commit

--- JS File/deposit.js
+++ JS File/deposit.js
@@ -14,8 +14,8 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     setTextElementValueById('deposit-total', newDepositTotal);
 
     // get previous balance by using the function
-    const previousBalanceTotal=getTextElementValueById('balance-total');
-    const newBalanceTotal=previousBalanceTotal+newDepositAmount;
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
 
-    setTextElementValueById('balance-total',newBalanceTotal);
+    setTextElementValueById('balance-total', newBalanceTotal);
 })
--- JS File/withdraw.js
+++ JS File/withdraw.js
@@ -8,14 +8,14 @@
 6.calculate new balance total
 7.set balance total using setTextElementValueById
 */
-document.getElementById('btn-withdraw').addEventListener('click',function(){
-    const newWithdrawAmount=getInputFieldValueById('withdraw-field');
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    const newWithdrawAmount = getInputFieldValueById('withdraw-field');
     // console.log(newWithdrawAmount)
-    const previousWithdrawTotal=getTextElementValueById('withdraw-total');
-    const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmount;
-    setTextElementValueById('withdraw-total',newWithdrawTotal);
-    const previousBalanceTotal=getTextElementValueById('balance-total');
-    const newBalanceTotal=previousBalanceTotal-newWithdrawAmount;
-    setTextElementValueById('balance-total',newBalanceTotal);
+    const previousWithdrawTotal = getTextElementValueById('withdraw-total');
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    setTextElementValueById('withdraw-total', newWithdrawTotal);
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
 
 })
